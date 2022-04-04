58becd95099fa58efdac280644d320e4193025c8
Milo Juarez <ermilo98go@gmail.com>
2022-04-04 12:34:04 -0500

    Moved folders, installed prettier and tailwindcss

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    printWidth: 200,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+};
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: { ...colors },
+        },
+    },
+    plugins: [],
+};
